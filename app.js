6e924399d08167804a17b607b63c5562729843db
mwinamijr <mwinamijr@gmail.com>
2022-12-08 00:16:50 +0300

    modified the app to use routes

--- app.js
+++ app.js
@@ -1,108 +1,30 @@
 require("dotenv").config();
 require("./config/database").connect();
 
-const bcrypt = require("bcryptjs/dist/bcrypt");
-const jwt = require("jsonwebtoken")
 const express = require("express");
-const auth = require("./middleware/auth")
-
 const app = express()
+
+const userRoute = require("./routes/userRoutes")
 
 app.use(express.json())
 
-// Logic goes here
-const User = require("./model/user")
+//routes to handle requests
+app.use("/users", userRoute)
 
-//Register
-app.post("/register", async (req, res) => {
-  //our register logic goes here
-  try {
-    // get user input
-    const { first_name, last_name, email, password } = req.body;
-
-    // validate user input
-    if (!(email && password && first_name && last_name )) {
-      res.status(400).send("All input is required")
-    }
-
-    // check if user already exists
-    // validate if user exist in our database
-
-    const oldUser = await User.findOne({ email });
-
-    if ( oldUser ) {
-      return res.status(409).send(" User already exist. Please login!")
-    }
-
-    // encrypt user password
-    encryptedPassword = await bcrypt.hash(password, 10);
-
-    //create user in our database
-    const user = await User.create({
-      first_name: first_name.toLowerCase(),
-      last_name: last_name.toLowerCase(),
-      email: email.toLowerCase(),
-      password: encryptedPassword,
-    });
-
-    // create token
-    const token = jwt.sign(
-      { user_id: user._id, email },
-      process.env.TOKEN_KEY,
-      {
-        expiresIn: "2h",
-      }
-    );
-
-    // save user token
-    user.token = token;
-
-    // return new user
-    res.status(201).json(user);
-  } catch (err) {
-    console.log(err)
-  }
-    
+//handle error requests
+app.get("/", (req, res) => {
+  res.send('API IS NOW WORKING, append "/docs" to the current url to access API documentation')
 })
 
-//Login
-app.post("/login", async (req, res) => {
-  //our login logic goes here
-  try {
-    // get user input
-    const { email, password } = req.body;
-
-    // validate user input 
-    if (!(email && password)) {
-      res.status(400).send("All input is required!")
-    }
-    // validate if user exist in our database
-    const user = await User.findOne({ email });
-
-    if (user && ( await bcrypt.compare(password, user.password))) {
-      // create token
-      const token = jwt.sign(
-        { user_id: user._id, email },
-        process.env.TOKEN_KEY,
-        {
-          expiresIn: "2h",
-        }
-      );
-
-      // save user token
-      user.token = token;
-
-      //user
-      res.status(200).json(user);
-    }
-    res.status(400).send("Invalid credentials!")
-  } catch (err) {
-    console.log(err)
-  }
+app.use((req, res, next) => {
+  const error = new Error()
+  error.message = "Not Found"
+  error.status = 404
+  next(error)
 })
 
-app.post("/welcome", auth, (req, res) => {
-  res.status(200).send("Welcome !!!")
+app.use((error, req, res, next) => {
+  res.status(error.status || 500).json({ error: error})
 })
 
 module.exports = app

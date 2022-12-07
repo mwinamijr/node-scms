c3ea1a72a65ac8d4c4a103e4319976a72db863bb
mwinamijr <mwinamijr@gmail.com>
2022-12-08 00:16:29 +0300

    created user controllers for the routes

---
+++ controllers/userController.js
@@ -0,0 +1,94 @@
+const bcrypt = require("bcryptjs/dist/bcrypt");
+const jwt = require("jsonwebtoken")
+const auth = require("../middleware/auth")
+
+// Logic goes here
+const User = require("../model/user")
+
+//Register
+exports.register = async (req, res) => {
+  //our register logic goes here
+  try {
+    // get user input
+    const { first_name, last_name, email, password } = req.body;
+
+    // validate user input
+    if (!(email && password && first_name && last_name )) {
+      res.status(400).send("All input is required")
+    }
+
+    // check if user already exists
+    // validate if user exist in our database
+
+    const oldUser = await User.findOne({ email });
+
+    if ( oldUser ) {
+      return res.status(409).send(" User already exist. Please login!")
+    }
+
+    // encrypt user password
+    encryptedPassword = await bcrypt.hash(password, 10);
+
+    //create user in our database
+    const user = await User.create({
+      first_name: first_name.toLowerCase(),
+      last_name: last_name.toLowerCase(),
+      email: email.toLowerCase(),
+      password: encryptedPassword,
+    });
+
+    // create token
+    const token = jwt.sign(
+      { user_id: user._id, email },
+      process.env.TOKEN_KEY,
+      {
+        expiresIn: "2h",
+      }
+    );
+
+    // save user token
+    user.token = token;
+
+    // return new user
+    res.status(201).json(user);
+  } catch (err) {
+    console.log(err)
+  }
+    
+}
+
+//Login
+exports.login = async (req, res) => {
+  //our login logic goes here
+  try {
+    // get user input
+    const { email, password } = req.body;
+
+    // validate user input 
+    if (!(email && password)) {
+      res.status(400).send("All input is required!")
+    }
+    // validate if user exist in our database
+    const user = await User.findOne({ email });
+
+    if (user && ( await bcrypt.compare(password, user.password))) {
+      // create token
+      const token = jwt.sign(
+        { user_id: user._id, email },
+        process.env.TOKEN_KEY,
+        {
+          expiresIn: "2h",
+        }
+      );
+
+      // save user token
+      user.token = token;
+
+      //user
+      res.status(200).json(user);
+    }
+    res.status(400).send("Invalid credentials!")
+  } catch (err) {
+    console.log(err)
+  }
+}

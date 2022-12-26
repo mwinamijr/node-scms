64a0e7e3ee5372094a48e7a11ab85c1fa61a5e82
mwinamijr <mwinamijr@gmail.com>
2022-12-26 20:39:41 +0300

    fixed send sms numbers for the smsSend controller

--- controllers/notificationController.js
+++ controllers/notificationController.js
@@ -1,8 +1,24 @@
+const AfricasTalking = require("africastalking")
+
+const africastalking = AfricasTalking({
+	apiKey: process.env.AFRICAS_TALKING_SANDBOX_API_KEY,
+	username: 'sandbox'
+})
+
+// Send SMS
 exports.sendSms = async (req, res) => {
-	console.log(req.body)
+	
 	try {
-		conslole.log(req.body)
+		console.log(req.body.employees)
+		const result = await africastalking.SMS.send({
+			to: req.body.employees,
+			message: req.body.message,
+			from: process.env.SENDER
+		})
+		console.log(result)
+		return res.status(200).send({message: "Message was sent successfully!", result: result})
 	} catch (error) {
-		return res.status(400).send({ message: "Sms was no sent!", error: error})
+		console.log(error)
+		return res.status(400).send({ message: "Message was no sent!", error: error})
 	}
 }
